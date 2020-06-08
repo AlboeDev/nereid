929558076d53aa59eb934970cddd3b8555a83149
interactivetimmy <interactivetimmy@gmail.com>
2020-06-07 20:57:30 -0400

    feat: generate template index file

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+
+/**
+ * The Entry class, this is a temporary test.
+ *
+ * @public
+ */
+export default class Entry {
+  /**
+   * Temporary value for x.
+   */
+  protected x: number;
+
+  /**
+   * Temporary Value for y.
+   */
+  protected y: number;
+
+  /**
+   * Constructs a new Entry. This is a temporary test.
+   */
+  public constructor() {
+    this.x = 1;
+    this.y = 2;
+  }
+}
