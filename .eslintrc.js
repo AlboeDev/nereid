600b070882aa36817f96373c22995e6b6a86d2c1
interactivetimmy <interactivetimmy@gmail.com>
2020-06-07 20:52:21 -0400

    chore(eslint): create config

---
+++ .eslintrc.js
@@ -0,0 +1,93 @@
+module.exports = {
+    env: {
+      es2020: true,
+      'jest/globals': true,
+      node: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+      'plugin:import/typescript',
+      'plugin:jsdoc/recommended',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    ignorePatterns: [
+      'rollup.config.js',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      '@typescript-eslint',
+      'jest',
+      'jsdoc',
+    ],
+    rules: {
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          ts: 'never',
+        },
+      ],
+      'import/prefer-default-export': 0,
+      'jest/no-hooks': [
+        'error',
+        {
+          allow: [
+            'afterAll',
+            'afterEach',
+            'beforeAll',
+            'beforeEach',
+          ],
+        },
+      ],
+      'jsdoc/check-tag-names': [
+        1,
+        {
+          definedTags: [
+            'internal',
+          ],
+        },
+      ],
+      'jsdoc/no-types': 1,
+      'jsdoc/no-undefined-types': 0,
+      'jsdoc/require-description': 1,
+      'jsdoc/require-description-complete-sentence': 1,
+      'jsdoc/require-hyphen-before-param-description': 1,
+      'jsdoc/require-jsdoc': [
+        2,
+        {
+          require: {
+            ArrowFunctionExpression: true,
+            ClassDeclaration: true,
+            ClassExpression: true,
+            FunctionDeclaration: true,
+            FunctionExpression: true,
+            MethodDefinition: true,
+          },
+        },
+      ],
+      'jsdoc/require-param-type': 0,
+      'jsdoc/require-returns-type': 0,
+      'jsdoc/valid-types': 1,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.js', '.ts'],
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.ts'],
+          paths: ['src', 'test'],
+        },
+      },
+    },
+  };
+  
